23b0f8e8c6d3b1e11ae25939fe29e42efe6e7746
Саид <HUEY.news@yandex.ru>
2024-12-07 13:22:18 +0300

    Перевёл проект на Single Activity.

---
+++ app/src/main/java/com/gootax/feedme/ui/MainFragment.kt
@@ -0,0 +1,25 @@
+package com.gootax.feedme.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.gootax.feedme.databinding.FragmentMainBinding
+import com.gootax.feedme.presentation.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
